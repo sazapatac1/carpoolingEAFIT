35d4e4cda329bbaf6f2552e8189329f3ca9a0bf4
sazapatac1 <saz4560@hotmail.com>
2019-04-11 13:49:56 -0500

    modificando a base de datos nube

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/carpoolingEAFIT',
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/carpoolingEAFIT',
     SECRET_TOKEN: 'miclavedetokens'
 }
